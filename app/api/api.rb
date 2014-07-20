cea0640e9284fd39c2bc3c15808a5ba68291efbe
matchy109 <masahiko.tokumaru@gmail.com>
2014-07-20 16:26:26 +0900

    changed url path pattern

--- app/api/api.rb
+++ app/api/api.rb
@@ -19,68 +19,80 @@ class API < Grape::API
     }.to_json, 404).finish
   end
 
-  rescue_from :all do |e|
+  rescue_from Grape::Exceptions::ValidationErrors do |e|
     Rack::Response.new({
       header: {
         status: e.status,
-        message: e.message
+        message: 'some mandatory parameters are missing'
       }
     }.to_json, e.status).finish
   end
 
+  rescue_from :all do |e|
+    Rack::Response.new({
+      header: {
+        status: 500,
+        message: e.message
+	#message: 'internal error'
+      }
+    }.to_json, 500).finish
+  end
+
+  after_validation do
+    authenticate!
+  end
+  
   params do
     requires :app_id,  type: Integer
     requires :app_key, type: String
   end
 
-  before do
-    authenticate!
+  resource :service do
+
+    resource :users do
+      params do
+        requires :name,     type: String
+        requires :birthday, type: Date
+      end
+
+      put "create", jbuilder:'new_user' do
+        @status = 200
+        @message = 'OK'
+        @user = User.create!({
+   	  name: params[:name],
+          birthday: params[:birthday]
+        })
+      end
+    end
+
+    resource :events do
+      params do
+        requires :user_id, type: Integer
+      end
+      get "/", jbuilder:'events' do
+        @status = 200
+        @message = 'OK'
+        @user = User.find(params[:user_id])
+      end
+
+      get ":id", jbuilder:'events' do
+        @user = User.find(params[:user_id])
+      end
+
+      params do
+        requires :user_id, type: Integer
+        requires :name,    type: String
+        requires :date,    type: Date
+      end
+      put "create", jbuilder:'new_event' do
+        @status = 200
+        @message = 'OK'
+        @user = Event.create!({
+  	  name: params[:user_id],
+	  name: params[:name],
+          date: params[:date]
+        })
+      end
+    end
   end
-
-  resource :users do
-    params do
-      requires :name,     type: String
-      requires :birthday, type: Date
-    end
-
-    put "create", jbuilder:'new_user' do
-      @status = 200
-      @message = 'OK'
-      @user = User.create!({
-	name: params[:name],
-        birthday: params[:birthday]
-      })
-    end
-  end
-
-  resource :events do
-    params do
-      requires :user_id, type: Integer
-    end
-    get "/", jbuilder:'events' do
-      @status = 200
-      @message = 'OK'
-      @user = User.find(params[:user_id])
-    end
-
-    get ":id", jbuilder:'events' do
-      @user = User.find(params[:user_id])
-    end
-
-    params do
-      requires :user_id, type: Integer
-      requires :name,    type: String
-      requires :date,    type: Date
-    end
-    put "create", jbuilder:'new_event' do
-      @status = 200
-      @message = 'OK'
-      @user = Event.create!({
-	name: params[:user_id],
-	name: params[:name],
-        date: params[:date]
-      })
-    end
-  end
-
 end
