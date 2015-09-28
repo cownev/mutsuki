0d5ac092faf3dafebbadef26e9457fe41c552f70
matchy109 <masahiko.tokumaru@gmail.com>
2015-09-27 23:17:17 -0600

    deleted unnecessary lines

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,8 +43,6 @@ RSpec.configure do |config|
 
   config.include JsonSpec::Helpers
   config.include RSpec::RequestDescriber, type: :request
-  #config.include RequestHelpers, type: :request
-  #config.include RequestMacros, type: :request
 
   config.before :all do
     FactoryGirl.reload
